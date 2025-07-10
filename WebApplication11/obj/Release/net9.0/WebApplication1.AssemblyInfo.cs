236bdaba721258ef98cdeb6efb828fb37b865579
= <martin.graf@outlook.de>
2025-07-10 21:12:12 +0200

    asdf

--- WebApplication11/obj/Release/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication11/obj/Release/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,9 +13,9 @@ using System.Reflection;
 
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("a1102841-8ef0-415d-8157-b846228262fc")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddee9d10e69b26a396a560c180648c15fc76c3c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0bc0bb84993e94dcba9b98247afe8710ebfbb87")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
